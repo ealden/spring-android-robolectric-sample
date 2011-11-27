790d5e5cbfdc936fafd4cd78f3c39ff1d08e9c4a
Ealden Esto E. Escanan <ealden@gmail.com>
2011-11-27 12:20:54 +0800

    Moved Activity to activity package
    Signed-off-by: Ealden Esto E. Escanan <ealden@gmail.com>

---
+++ src/main/java/net/ealden/android/activity/SpringAndroidRobolectricActivity.java
@@ -0,0 +1,13 @@
+package net.ealden.android.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import net.ealden.android.R;
+
+public class SpringAndroidRobolectricActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
