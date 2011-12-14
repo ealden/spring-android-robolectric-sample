eba0b4b14760bf76966689f884e349a95051ed2d
Ealden Esto E. Escanan <ealden@gmail.com>
2011-12-14 22:30:49 +0800

    Replaced AsyncTask with @Background and @UiThread
    Signed-off-by: Ealden Esto E. Escanan <ealden@gmail.com>

--- client/src/main/java/net/ealden/android/activity/SpringAndroidRobolectricActivity.java
+++ client/src/main/java/net/ealden/android/activity/SpringAndroidRobolectricActivity.java
@@ -1,36 +1,50 @@
 package net.ealden.android.activity;
 
 import android.app.Activity;
-import android.os.AsyncTask;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
+import com.googlecode.androidannotations.annotations.Background;
 import com.googlecode.androidannotations.annotations.Click;
 import com.googlecode.androidannotations.annotations.EActivity;
+import com.googlecode.androidannotations.annotations.UiThread;
 import com.googlecode.androidannotations.annotations.ViewById;
 import net.ealden.android.R;
+import org.springframework.web.client.RestClientException;
 
 import static net.ealden.android.RestTemplateFactory.*;
 
 @EActivity(R.layout.main)
 public class SpringAndroidRobolectricActivity extends Activity {
+    protected static final String TAG = SpringAndroidRobolectricActivity.class.getSimpleName();
+
     @ViewById(R.id.outputArea)
     TextView outputArea;
 
     @Click(R.id.callWebService)
     public void callWebService() {
-        new GetAsyncTask().execute();
+        retrieveData();
     }
 
-    private class GetAsyncTask extends AsyncTask<Void, Void, String> {
-        @Override
-        protected String doInBackground(Void... voids) {
-            String result = getRestTemplate().getForObject("http://10.0.2.2:8080/server", String.class);
+    @Background
+    void retrieveData() {
+        String result = null;
 
-            return result;
+        try {
+            result = getRestTemplate().getForObject("http://10.0.2.2:8080/server", String.class);
+        } catch (RestClientException e) {
+            Log.e(TAG, "Unable to connect to webservice", e);
         }
 
-        @Override
-        protected void onPostExecute(final String result) {
+        displayData(result);
+    }
+
+    @UiThread
+    void displayData(final String result) {
+        if (result == null) {
             outputArea.setText(result);
+        } else {
+            Toast.makeText(SpringAndroidRobolectricActivity.this, "Unable to connect to webservice.", Toast.LENGTH_LONG).show();
         }
     }
 }
